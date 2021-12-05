d17dd94eb203587ec82f5503977951d0bab93821
NorayrHay <hayrikyannoro@gmail.com>
2021-12-06 03:17:00 +0400

    feat: i added my file and did ellCalculation task

---
+++ ellCalculation.js
@@ -0,0 +1,18 @@
+'use strict'
+
+function ellCalculation(array){
+    // let sum = 0;
+    // let diff = 0;
+    // let mult = 1;  
+  
+    for (let i = 0; i < array.length; i++) {
+      // sum += array[i];
+      // diff -= array[i];
+      // mult *= array[i];
+  
+      // else  return  'infinity';
+    }
+    // res = '' + sum + diff + mult + div; 
+    // return div;
+  }
+  console.log(ellCalculation([2, 0, -7, 10]));
